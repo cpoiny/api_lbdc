fd5a5f6dbdd7560201987927df1881c86edb57ba
cpoiny <cynthia.poiny@gmail.com>
2024-05-03 15:03:46 +0000

    WIP sur le CRUD Author

--- src/controllers/AuthorController.ts
+++ src/controllers/AuthorController.ts
@@ -6,12 +6,14 @@ class AuthorController {
 
     private authorService = new AuthorService();
 
+    // OK - GET ALL users
     async getAll(req: Request, res: Response) {
         console.log("AuthorController");
         const authors = await this.authorService.getAll();
         res.send({ status: "OK", data: authors })
     }
 
+    // OK - GET AUTHOR BY ID
     async getAuthorById(req: Request, res: Response) {
         console.log("AuthorController - get by id");
 
@@ -23,16 +25,41 @@ class AuthorController {
         }
     }
 
-
+    // OK CREATE AUTHOR
     async create(req: Request, res: Response) {
         console.log("AuthorController create");
 
         try {
             const author = await this.authorService.create(req.body);
             res.send({ status: "OK", data: author });
+        } catch (error: unknown) {
+            let  errorMessage = (error as Error).message
+            res.status(500).send({ status: "Failed", message: errorMessage });
+        }
+    }
+
+     //ok - UPDATE USER
+     async updateAuthor(req: Request, res: Response) {
+        console.log("AuthorController - update");
+
+        try {
+            await this.authorService.updateAuthor(Number(req.params.id), req.body);
+            res.send({ status: 200, message: "Success to update author" });
+        } catch (error: unknown) {
+           let  errorMessage = (error as Error).message
+            res.status(500).send({ status: 500 , message: `Failed to update author because ${errorMessage}`  });
+        }
+    }
+
+ // OK DELETE AUTHOR
+    async deleteAuthor(req: Request, res: Response) {
+        console.log("USerController - delete");
+
+        try {
+            await this.authorService.deleteAuthor(Number(req.params.id));
+            res.send({ status: "OK", message: `Sucess to delete author!` });
         } catch (error) {
-            console.log("mon erreur", error);
-            res.status(500).send({ status: "Failed", message: error });
+            res.status(500).send({ status: 500, message: `Failed to delete author or author doesn't found!`});
         }
     }
 }
--- src/routes/AuthorRoutes.ts
+++ src/routes/AuthorRoutes.ts
@@ -17,13 +17,23 @@ authorRouter.get("/:id", (req, res) => {
 });
 
 
-
 // ok - ajouter le checkToken pour le update, delete des author et aussi pour author et media
 authorRouter.post("/ajouter", (req, res) => {
     console.log("AuthorRouter create");
     authorController.create(req,res);
 });
 
+// ok - PUT - UPDATE AUTHOR
+authorRouter.put("/:id", (req, res) => {
+    console.log("UserRouter - update");
+    authorController.updateAuthor(req,res);
+});
+
+// ok - DELETE - AUTHOR
+authorRouter.delete("/:id", (req, res) => {
+    console.log("authorRouter - delete");
+    authorController.deleteAuthor(req,res);
+});
 
 // on exporte pour qu'il puisse etre appelé par index.ts
 export default authorRouter;
