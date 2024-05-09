8955b8930c7e0836187f405b334ed8940078d27f
cpoiny <cynthia.poiny@gmail.com>
2024-05-09 08:48:57 +0000

    update model pour creation d'un post

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -34,18 +34,15 @@ export class Post {
     @Column({nullable:true})
     quantity_likes?: number
 
-    @ManyToOne(()=> User, (user) => user.posts,
-    {nullable: false})
+    @ManyToOne(()=> User, (user) => user.posts,{nullable: false})
     @JoinColumn({ name: "user_id"})
     user_id!: User['id']
 
-    @OneToMany(()=> Comment, 
-        (comment) => comment.post_id, {nullable:true}
-    )
+    @OneToMany(()=> Comment, (comment) => comment.post_id, {nullable:true})
     comments?: Comment[]
 
     // Si le media est delete, update alors le post aussi
-    @ManyToMany(() => Media, {onDelete: "CASCADE", onUpdate: "CASCADE"})
+    @ManyToMany(() => Media, media => media.posts, {onDelete: "CASCADE", onUpdate: "CASCADE"})
     @JoinTable({name : "post_media",
     joinColumn: { name: 'post_id', referencedColumnName: 'id' },
     inverseJoinColumn: { name: 'media_id', referencedColumnName: 'id' }})
--- src/modelsDTO/post.dto.ts
+++ src/modelsDTO/post.dto.ts
@@ -8,14 +8,13 @@ export class PostDTO {
     title!: string;
     content!: string;
     picture!: string;
-    updated_at!: Date;
     publicated_at!: Date;
+    updated_at!: Date;
     is_draft!: boolean;
-    quantity_comments!: number;
-    quantity_likes!: number;
-    comments!: CommentDTO[];
-    media!: MediaDTO;
-    author!: AuthorDTO;
     user_id!: number;
-
+    author!: AuthorDTO;
+    media?: MediaDTO;
+    comments?: CommentDTO[];
+    quantity_comments?: number;
+    quantity_likes?: number;
 }
