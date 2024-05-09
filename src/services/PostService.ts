2a6e07b2285456ac803d59cc04eb19dbcb407aba
cpoiny <cynthia.poiny@gmail.com>
2024-05-09 14:17:37 +0000

    refactorisation de la methode create post

--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -2,6 +2,8 @@ import AppDataSource from "../data-source";
 import { Author } from "../entities/Author";
 import { Media } from "../entities/Media";
 import { Post } from "../entities/Post";
+import { AuthorAndMediaDTO } from "../modelsDTO/AuthorAndMedia.dto";
+import { MediaDTO } from "../modelsDTO/media.dto";
 import { PostDTO } from "../modelsDTO/post.dto";
 import AuthorService from "./AuthorService";
 import MediaService from "./MediaService";
@@ -12,19 +14,16 @@ class PostService {
     private mediaRepository = AppDataSource.getRepository(Media);
     private authorService = new AuthorService();
     private mediaService = new MediaService();
+
     async getAll() {
         console.log("PostService - get all");
         return this.postRepository.find();
     }
 
-
     // Creation d'un post et de ses liaisons -- ok
     async create(post: PostDTO) {
         console.log("PostService - create");
 
-        //  Créer un nouveau post de type POST (en entrée on recupére une requete contenant un PostDTO, c'est a dire le  formumlaire du front qui contient toutes les données pour créer un auteur, un média et un post)
-
-        // 1 je crée la structure de mon post
         const newPost = new Post();
         newPost.title = post.title;
         newPost.content = post.content;
@@ -35,66 +34,67 @@ class PostService {
         newPost.quantity_comments = post.quantity_comments;
         newPost.quantity_likes = post.quantity_likes;
 
-        // 2 - verifier si l'auteur existe  déjà dans la base de données
-        const author = await this.authorRepository.findOneBy({ name: post.author.name });
-
-        // 3 - l'auteur n'existe pas, je le crée et je le lie au post
-        if (author === null) {
-            const newAuthor = await this.authorService.create(post.author);
-
-            // 4- je crée un média (car un média existe uniquement si un auteur et un post existent)
-            const newMedia = new Media();
-            // je recupère les données du post recu en entrée
-            newMedia.title = post.media?.title;
-            newMedia.category = post.media?.category;
-            newMedia.theme = post.media?.theme;
-            newMedia.edition = post.media?.edition;
-            // je recupère l'auteur id de l'auteur que je viens de créer
-            newMedia.author_id = newAuthor.id!;
-
-            //5 - je créé le média dans la base de données
-            const newMediaToSave = await this.mediaService.create(newMedia);
-
-            // 6- je lie l'auteur et le média au post
-            newPost.authors = [newAuthor];
-            newPost.medias = [newMediaToSave];
-
-            // 7 - l'auteur existe déja
-        } else {
-
-            // je le mets à jour (car j'ai toutes ces infos dans le post recu en entrée)
-            const isAuthorUpdated = await this.authorRepository.update(author.id!, post.author);
-
-            // si l'auteur a été mis à jour, je le recupere pour le lier au post
-            if (isAuthorUpdated) {
-                const updatedAuthor = await this.authorRepository.findOneBy({ id: author.id });
-                // je recupére le média de l'auteur
-                const media = await this.mediaRepository.findOneBy({ title: post.media?.title });
-
-                if(media === null) {
-
-                    const newMedia = new Media();
-                    newMedia.title = post.media?.title;
-                    newMedia.category = post.media?.category;
-                    newMedia.theme = post.media?.theme;
-                    newMedia.author_id = updatedAuthor!.id!;
-                    const newMediaToSave = await this.mediaService.create(newMedia);
-        
-                    
-                    newPost.authors = [updatedAuthor!];
-                    newPost.medias = [newMediaToSave];
-                } else {
-                    // liaison de l'auteur au post
-                    newPost.authors = [updatedAuthor!];
-                    // liaison du média au post
-                    newPost.medias = [media];
-                }
-
-                }
+        let results = await this.createOrUpdateAuthor(post);
+        if (results) {
+            newPost.authors = [results.author!];
+            newPost.medias = [results.media!];
         }
         // 8 - je crée le post dans la base de données
         const newPostToSave = this.postRepository.create(newPost);
         return await this.postRepository.save(newPostToSave);
+    }
+
+    // Create or UpdateAuthor
+    async createOrUpdateAuthor(post: PostDTO) {
+        let results = new AuthorAndMediaDTO();
+        const existingAuthor = await this.authorRepository.findOneBy({ name: post.author.name });
+
+        if (existingAuthor === null) {
+            const newAuthor = await this.authorService.create(post.author);
+            results.author = newAuthor;
+            if (newAuthor) {
+                const newMedia = await this.createMedia(post.media!, newAuthor.id!);
+                results.media = newMedia
+            }
+            return results;
+
+        } else {
+
+            const isAuthorUpdated = await this.authorRepository.update(existingAuthor.id!, post.author);
+            if (isAuthorUpdated) {
+                const updatedAuthor = await this.authorRepository.findOneBy({ id: existingAuthor.id });
+                if(updatedAuthor) {
+                    const media = await this.mediaRepository.findOneBy({ title: post.media?.title });
+                    if ( media === null) {
+                        const newMedia = await this.createMedia(post.media!, updatedAuthor.id!);
+                        results.author = updatedAuthor;
+                        results.media = newMedia;
+                        return results;   
+                    } else {
+                        const isUpdatedMedia = await this.mediaRepository.update(media.id!, media);
+                        if (isUpdatedMedia){
+                            const updatedMedia = await this.mediaRepository.findOneBy({ id: media.id });
+                            results.author = updatedAuthor;
+                            results.media = updatedMedia!;
+                        }
+                }   
+                } else {
+                    throw new Error ('Something went wrong while updating author');
+                }
+            }   
+        }
+    }
+
+    // ccreate or update media
+    async createMedia(media: MediaDTO, id: number) {
+        const newMedia = new Media();
+        newMedia.title = media.title;
+        newMedia.category = media.category;
+        newMedia.theme = media.theme;
+        newMedia.edition = media?.edition;
+        newMedia.author_id = id!;
+        const newMediaToSave = await this.mediaService.create(newMedia);
+        return newMediaToSave;
     }
 
 }
